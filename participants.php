5db4f0eb422758fc285ea28977cf2d107569541f
mimie <karen.convicto@yahoo.com>
2013-07-19 19:13:32 +0800

    added script to check all checkboxes

--- participants.php
+++ participants.php
@@ -2,6 +2,31 @@
 <head>
 <title>Participants</title>
 <link rel="stylesheet" type="text/css" href="design.css" />
+<script type="text/javascript">
+  var checked=false;
+  var formname='';
+
+function checkedAll(formname)
+{
+  var values= document.getElementById(formname);
+ 
+  if (checked==false)
+  {
+    checked=true;
+  }
+ 
+  else
+  {
+    checked = false;
+  }
+
+ for (var i =0; i < values.elements.length; i++)
+ {
+   values.elements[i].checked=checked;
+ }
+
+}
+</script>
 </head>
 <body>
 <?php
