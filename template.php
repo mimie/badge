c789fc3b1ff1d923087cf1bdb92e66af40185658
mimie <karen.convicto@yahoo.com>
2013-07-29 14:12:05 +0800

    change height and width of td in event title and logo

--- template.php
+++ template.php
@@ -19,11 +19,11 @@ table{
 <div id = 'badge'>
  <table>
   <tr>
-  <td align='center' width='22%'><img src='iiap_logo.png' width='100' height='111'></td>
-   <td align='left' width='78%' height='111' cellpadding='3px'><font size='4'><h3>66th Annual Conference</h3></td>
+  <td align='center' width='100' height='111'><img src='iiap_logo.png' width='100' height='111'></td>
+   <td align='left' height='111' cellpadding='3px'><font size='4'><h3>66th Annual Conference</h3></td>
   </tr>
   <tr>
-    <td colspan=2 align='center'><b><font size=7>Helen De Guzman</b></br></font>Manila Electric Company</td>
+    <td colspan=2 align='center'><b><font size='7'>Helen De Guzman</b></br></font><font size='5'>Manila Electric Company</font></td>
   </tr>
   <tr>
     <td colspan=2 align='right'><font size='5'>20 Jul 2013&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
