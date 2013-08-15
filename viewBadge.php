0d7604e6a8116c4b841d0132d8869e22078a3d62
mimie <karen.convicto@yahoo.com>
2013-08-15 11:49:46 +0800

    display bades in another page

--- viewBadge.php
+++ viewBadge.php
@@ -1,4 +1,29 @@
 <?php
-  $ids = urldecode($_REQUEST['ids']);
-  $ids = json_decode($ids);
+
+  include 'dbcon.php';
+  include 'badges_functions.php';
+
+  
+  $contactIds = urldecode($_REQUEST['ids']);
+  $contactIds = json_decode($contactIds);
+
+  $eventId = urldecode($_REQUEST['eventId']);
+  $eventId = json_decode($eventId);
+
+  $properties = array();
+  $properties["bHeight"] = '205px';                                                                                                                                  
+  $properties["bWidth"] = '329px';                                                                                                                                    
+  $properties["imgHeight"] = '77';                                                                                                                                      
+  $properties["imgWidth"] = '73';                                                                                                                                 
+  $properties["titleSize"] = '4';                                                               
+  $properties["nameSize"] = '5';                                                                                                                                                    
+  $properties["orgSize"] = '4';                                                                                                                                              
+  $properties["dateSize"] = '4';
+
+  foreach($contactIds as $id){
+
+    $participantDetails = getParticipantDetails($id);
+    $htmlBadge = htmlBadge($eventId,$participantDetails,$properties);
+    echo $htmlBadge;
+  }
 ?>
