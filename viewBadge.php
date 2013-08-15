61be8a9a0c3da90dd3e60390b6e987dd1122da39
mimie <karen.convicto@yahoo.com>
2013-08-15 15:00:46 +0800

    put badges in table

--- viewBadge.php
+++ viewBadge.php
@@ -20,10 +20,27 @@
   $properties["orgSize"] = '4';                                                                                                                                              
   $properties["dateSize"] = '4';
 
+  echo "<table>";
+  $indicator = 1;
+
   foreach($contactIds as $id){
 
     $participantDetails = getParticipantDetails($id);
     $htmlBadge = htmlBadge($eventId,$participantDetails,$properties);
-    echo $htmlBadge;
+    //echo $htmlBadge;
+
+    if($indicator == 1){
+      echo "<tr>";
+      echo "<td>$htmlBadge<td>";
+      $indicator = $indicator + 1;
+    }
+
+    elseif($indicator == 2){
+      echo "<td>$htmlBadge</td></tr>"; 
+      $indicator = 1;
+    }
+    
   }
+
+ echo "</table>";
 ?>
