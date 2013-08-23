b3788c8b2781595084d50c40ac72b7be953580b6
mimie <karen.convicto@yahoo.com>
2013-08-23 16:44:43 +0800

    added table for eventInfo

--- eventList.php
+++ eventList.php
@@ -1,7 +1,50 @@
+<?php
+  include 'dbcon.php';
+  include 'badges_functions.php';
+?>
 <html>
 <head>
 <title>Event List</title>
+<style>
+#eventHeader{
+  text-align:center;
+  padding:5px;
+}
+
+#eventDetails{
+  padding:5px;
+}
+</style>
 </head>
 <body>
+<div style="height:5px;border:1px solid #0489B1;background:#0489B1;"></div>
+<div id="eventHeader">
+<h4>Institute of Internal Auditors - Philippines<br>
+Centre for Professional Development<br>
+Attendance and CPE Form</h4>
+</div>
+<div style="height:5px;border:1px solid #0489B1;background:#0489B1;"></div>
+
+<div id="eventDetails" align="center">
+ <table id="">
+  <tr>
+   <td><b>Topic</b></td>
+   <td>BUSINESS PROCESSING MAPPING</td>
+  </tr>
+  <tr>
+   <td><b>Date, Time</b></td>
+   <td>22 JUNE 2013</td>
+  </tr>
+  <tr>
+   <td><b>Speaker</b></td> 
+   <td>Mags V. Mendez III, CPA, CIA, CCSA, CRMA</td>
+  </tr>
+  <tr>
+   <td><b>Venue</b></td>
+   <td>ST. GILES HOTEL, MAKATI CITY</td>
+  </tr>
+ </table>
+</div>
+
 </body>
 </html>
