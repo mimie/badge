32245186f47752e8bcb5facf25a3a5d7742c599d
mimie <karen.convicto@yahoo.com>
2013-09-10 14:04:31 +0800

    test getParticipantByEvent and getParticipantStatusId function

--- weberp/weberp_test.php
+++ weberp/weberp_test.php
@@ -13,4 +13,10 @@
   $searchEvent3 = searchEventName("karen");
   var_dump($searchEvent3);
 
+  $displayParticipants = getParticipantByEvent("139");
+  echo $displayParticipants;
+
+  $statusId = getParticipantStatusId("7831","139");
+  var_dump($statusId);
+
 ?>
