b822377fb973665344a189887b4a2cf4c5c47498
mimie <karen.convicto@yahoo.com>
2013-09-11 11:09:01 +0800

    test displayEventHeader function

--- weberp/weberp_test.php
+++ weberp/weberp_test.php
@@ -13,8 +13,8 @@
   $searchEvent3 = searchEventName("karen");
   var_dump($searchEvent3);
 
-  $displayParticipants = getParticipantByEvent("139");
-  echo $displayParticipants;
+  /*$displayParticipants = getParticipantByEvent("139");
+  echo $displayParticipants;**/
 
   $statusId = getParticipantStatusId("7831","139");
   var_dump($statusId);
@@ -24,5 +24,8 @@
 
   $statusTypeSelectForm = statusTypeSelectForm("3");
   echo $statusTypeSelectForm;
+
+  $eventHeader = displayEventHeader("139");
+  echo $eventHeader;
 
 ?>
