f463a7ca6b3c61a8104d6c3503623b2f4fa569b3
mimie <karen.convicto@yahoo.com>
2013-09-11 09:59:46 +0800

    test getParticipantByEvent

--- weberp/weberp_test.php
+++ weberp/weberp_test.php
@@ -13,8 +13,8 @@
   $searchEvent3 = searchEventName("karen");
   var_dump($searchEvent3);
 
-  /**$displayParticipants = getParticipantByEvent("139");
-  echo $displayParticipants;**/
+  $displayParticipants = getParticipantByEvent("139");
+  echo $displayParticipants;
 
   $statusId = getParticipantStatusId("7831","139");
   var_dump($statusId);
