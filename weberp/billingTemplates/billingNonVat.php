9fe20e1411b2e9f5965df7a90f0c01ce0117a562
mimie <karen.convicto@yahoo.com>
2013-09-30 12:53:50 +0800

    added template for billing with no vat

---
+++ weberp/billingTemplates/billingNonVat.php
@@ -0,0 +1,216 @@
+<html>
+<head>
+<title>Billing With Non Vat</title>
+<style>
+#main{
+  border: 3px solid #0000FF;
+  width: 739.28px;
+  height: 959.62px;
+
+}
+
+#header{
+  width: 699.21px;
+  height: 49.51px;
+  background-color: #08088A;
+  font-family: Arial;
+  color: white;
+  font-weight: bold;
+}
+
+#logo{
+  width:48.38px;
+  height:43.09px;
+  padding: 2px 2px 2px 2px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#tin{
+  width: 699.21px;
+  height: 57.83px;
+  font-family: Arial;
+  margin: margin 0 auto;
+  padding: 3px 1px 1px 1px;
+  
+}
+#billedTo{
+  width: 699.21px;
+  font-family: Arial;
+  margin: margin 0 auto;
+  
+}
+</style>
+</head>
+<body>
+<div id="main">
+  <div style="width:896.5px;height:7.93px;"></div>
+   <center>
+    <div id="header">
+     <table id="header">
+       <tr>
+        <td rowspan="2"><img id="logo" src="iiap_logo.png"></td>
+        <td>Institute of Internal Auditors Philippines, Inc.</td>
+       </tr>
+      <tr>
+        <td><font size="2">Unit 702 Corporate Center, 139 Valero St., Salcedo Village, Makati City 1227</font></td>
+      </tr>
+    </table>
+   </div>
+  </center>
+  <center>
+    <div id="tin">
+      <table align="left">
+       <tr>
+         <td width="50%"><font style="font-size:12px"><b>TIN No. 001-772-403-000 : (+632) 940-9551 /940-9554 : Fax (+632) 325-0414</b></font></td>
+         <td width="50%" align="right" rowspan="2"><font style="font-size:48px;color:#0B3861;"><i>BILLING</i></font></td>
+       </tr>
+       <tr><td><br></td></tr>
+     </table>
+    </div>
+    <!--billed to-->
+    <div id="billedTo">
+     <table align="left">
+      <tr>
+         <td width="101.67px"><font style="font-size:16px"><b>BILLED TO:</b></font></td>
+         <td width="329.95px"><font style="font-size:13px"><b>[Company Name]</b></font></td>
+         <td width="135.31px" style="border-right:2px solid black;"><font style="font-size:13px"><b>DATE:</b></font></td>
+         <td width="132.28px"><font style="font-size:13px">September 27, 2013</font></td>
+      </tr>
+      <tr>
+         <td></td>
+         <td><font style="font-size:13px">[Street Address]</font></td>
+         <td style="border-right:2px solid black;"><font style="font-size:15px"><b>Billing No.:</b></font></td>
+         <td><font style="font-size:13px"><b>[100]</b></font></td>
+      </tr>
+      <tr>
+         <td></td>
+         <td><font style="font-size:13px">[City, ST  ZIP Code]</font></td>
+         <td style="border-right:2px solid black;"><font style="font-size:13px"><b>Member ID:</b></font></td>
+         <td><font style="font-size:13px">[ABC12345]</font></td>
+      </tr>
+      <tr>
+         <td></td>
+         <td><font style="font-size:13px">[Phone]</font></td>
+         <td style="border-right:2px solid black;"></td>
+         <td></td>
+      </tr>
+      <tr>
+       <td colspan="3" height="37.8px" style="border-right:2px solid black;"><br></td>
+       <td></td>
+      </tr>
+     </table>
+     <!--end of billed to-->
+    </div>
+    <div id="billedTo">
+     <!--particulars-->
+     <table align="left" style="border-collapse:collapse;">
+      <tr>
+        <td colspan="2" width="566.93px" align="center" bgcolor="#0B0B3B" style="border:2px solid #BCF5A9"><font style="font-size:13px;color:white;"><b>PARTICULARS</b></font></td>
+        <td width="132.28px" align="center" bgcolor="#0B0B3B" style="border:2px solid #BCF5A9"><font style="font-size:13px;color:white;"><b>AMOUNT</b></font></td>
+      </tr>   
+      <tr>
+        <td colspan="2" height="350px" style="border:2px solid #BCF5A9;"></td>
+        <td style="border:2px solid #BCF5A9;"></td> 
+      </tr>
+      <!--
+      <tr>
+        <td width="527.71px" style="border:2px solid black;" rowspan="2" align="center">
+         <font style="font-size:19px"><b><i>THANK YOU FOR YOUR BUSINESS!</b></i></font><br>
+         <font style="font-size:13px"><b>(NOT VALID FOR INPUT TAX CLAIM)</b></font>
+        </td>
+        <td width="165px" style="border:2px solid black;" align="right" rowspan="2">SUBTOTAL<br>VAT - 12%</td>
+        <td width="195.78px" height="26.84px" style="border:2px solid black;"></td>
+      </tr>
+      <tr>
+        <td width="195.78px" height="26.84px" style="border:2px solid black;"></td>
+      </tr>
+      <tr>
+        <td colspan="2" height="15px"></td>
+        <td width="195.78px" style="border:2px solid black;" rowspan="2"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><b><font style="font-size:13px;font-family:Arial">DIRECT ALL INQUIRIES TO:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            PAYMENT INSTRUCTION:</font></b>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2"><b><i><font style="font-size:13px;font-family:Arial">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ARTHELEI V. MENDOZA</i></b>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         &Oslash;&nbsp;If by check, <font color="red"><b><u>should be</u></b></font> made payable to:</font>
+        </td>
+        <td align="center" width="195.78px" style="border:2px solid black;" bgcolor="#D8D8D8"><font style="font-size:11px"><b>TOTAL AMOUNT DUE</b></font></td>
+      </tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(+632) 940-9554</font></td>
+      </tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">email: ar_finance@iia-p.org</font>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <font style="font-size:22px"><b><i>Institute of Internal Auditors Philippines, Inc.</i></b></font>
+       </td>
+      </tr>
+      <tr><td><br></td></tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;
+          &Oslash;&nbsp;If thru bank telegraphic transfer, include <b><u>P250 /$ 6.50,</b></u> in your payment to cover for bank charges.
+        </font>
+       </td>
+      </tr>
+      <tr><td></td></tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;
+          &Oslash;&nbsp;By direct deposit to:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Institute of Intenal Auditors Philippines, Inc.</b>
+       </font>
+       </td>
+      </tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <b>Acct No. 691-002-1745</b>
+          </font>
+       </td>
+      <tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <b>Banco de Oro - Rufino Branch</b>
+          </font>
+       </td>
+      <tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          Please indicate the BDO branch where you are transacting and present your <b>ORIGINAL</b>
+          </font>
+       </td>
+      <tr>
+      <tr>
+       <td colspan="3"><font style="font-size:13px;font-family:Arial">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <b>COPY</b> of the bank validated deposit slip to claim your OFFICIAL RECEIPT.
+          </font>
+       </td>
+      <tr>-->
+     </table>
+    </div>
+  </center>
+</div>
+</body>
+</html>
